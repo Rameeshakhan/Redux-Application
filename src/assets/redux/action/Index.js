35544a80d3c746bb819169167f5e0230216de87f
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-03 01:45:55 +0500

    Remove cart action created

--- src/assets/redux/action/Index.js
+++ src/assets/redux/action/Index.js
@@ -7,9 +7,9 @@ export const addToCart = (data) => {
     
 }
 
-// export const removeFromCart = (data) => {
-//     return {
-//         type: REMOVE_FROM_CART,
-//         data: data,
-//     }
-// }
+export const removeFromCart = (data) => {
+    return {
+        type: REMOVE_FROM_CART,
+        data: data,
+    }
+}
